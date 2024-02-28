f646b8e7e4c96b742afc257771f4a3fbbcf128bb
faisaleric5530 <faisaleris5530@gmail.com>
2024-02-28 18:24:19 +0530

    changes added with integration of social links

--- script.js
+++ script.js
@@ -4,3 +4,8 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+function openInNewTab(url) {
+  var win = window.open(url, '_blank');
+  win.focus();
+}
